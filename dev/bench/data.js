a55a46d395065fec2174f6577dddee0e88641efa
github-action-benchmark <github@users.noreply.github.com>
2024-07-23 15:03:33 +0000

    add Benchmark (benchmarkjs) benchmark result for 81482ae7cf0c3b58e62a1e4acf63611038569503

---
+++ dev/bench/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1721747013496,
+  "repoUrl": "https://github.com/jenniferlianne/cacti",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "vramakr2@in.ibm.com",
+            "name": "VRamakrishna",
+            "username": "VRamakrishna"
+          },
+          "committer": {
+            "email": "vramakr2@in.ibm.com",
+            "name": "VRamakrishna",
+            "username": "VRamakrishna"
+          },
+          "distinct": true,
+          "id": "81482ae7cf0c3b58e62a1e4acf63611038569503",
+          "message": "chore(cleanup-docs): deleted last-remaining deprecated files and folders\n\nReflected pending changes to the new docs folder.\nUpdated com.jfrog.artifactory versions to fix missing dependency.\n\nSigned-off-by: VRamakrishna <vramakr2@in.ibm.com>",
+          "timestamp": "2024-07-23T14:12:54+05:30",
+          "tree_id": "57807bf092f668e20c2e16d28feab8ede8b8a24d",
+          "url": "https://github.com/jenniferlianne/cacti/commit/81482ae7cf0c3b58e62a1e4acf63611038569503"
+        },
+        "date": 1721747011117,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 556,
+            "range": "±1.72%",
+            "unit": "ops/sec",
+            "extra": "177 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 335,
+            "range": "±1.19%",
+            "unit": "ops/sec",
+            "extra": "179 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
