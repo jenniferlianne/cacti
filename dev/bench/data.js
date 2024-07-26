be1eb449f2c85ee7d2c14c3c5e684639b2034852
github-action-benchmark <github@users.noreply.github.com>
2024-07-26 15:01:03 +0000

    add Benchmark (benchmarkjs) benchmark result for 9c4d9be8ac7a1608bf15cbaf887ed0836c02f747

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722005188537,
+  "lastUpdate": 1722006063061,
   "repoUrl": "https://github.com/jenniferlianne/cacti",
   "entries": {
     "Benchmark": [
@@ -174,6 +174,37 @@ window.BENCHMARK_DATA = {
             "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
             "value": 324,
             "range": "±1.11%",
+            "unit": "ops/sec",
+            "extra": "179 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jagpreet.singh.sasan@accenture.com",
+            "name": "jagpreetsinghsasan",
+            "username": "jagpreetsinghsasan"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": false,
+          "id": "9c4d9be8ac7a1608bf15cbaf887ed0836c02f747",
+          "message": "build(api-client): generate go client\n\n    Primary Changes\n    ----------------\n    1. Updated package.json file for packages to\n       include the new codegen script\n    2. Added a new dep, replace for string\n       manupulation in package.json\n\nFixes #393\n\nSigned-off-by: jagpreetsinghsasan <jagpreet.singh.sasan@accenture.com>",
+          "timestamp": "2024-07-25T14:06:22-07:00",
+          "tree_id": "4db60a87b0df4cca6a76f559d461c607d148f604",
+          "url": "https://github.com/jenniferlianne/cacti/commit/9c4d9be8ac7a1608bf15cbaf887ed0836c02f747"
+        },
+        "date": 1722006060309,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "plugin-ledger-connector-besu_HTTP_GET_getOpenApiSpecV1",
+            "value": 657,
+            "range": "±3.22%",
             "unit": "ops/sec",
             "extra": "179 samples"
           }
