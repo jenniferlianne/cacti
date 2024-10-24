6e0aece3fcf5d97191e01dd0ad67de1ce1160cc1
github-action-benchmark <github@users.noreply.github.com>
2024-10-24 14:59:49 +0000

    add Benchmark (benchmarkjs) benchmark result for 891a70567243eb96879680e3deea1136d9ad654c

---
+++ dev/bench/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1729781989386,
+  "repoUrl": "https://github.com/jenniferlianne/cacti",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "aldousss.alvarez@gmail.com",
+            "name": "aldousalvarez",
+            "username": "aldousalvarez"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "891a70567243eb96879680e3deea1136d9ad654c",
+          "message": "test(test-plugin-ledger-connector-besu): jestify v21-sign-transaction-endpoint\n\nPrimary Changes\n----------------\npackages/cactus-test-plugin-ledger-connector-besu/src/test/typescript/\nintegration/plugin-validator-besu/v21-sign-transaction-endpoint.test.ts\n\nFixes #3565\n\nSigned-off-by: aldousalvarez <aldousss.alvarez@gmail.com>",
+          "timestamp": "2024-10-22T09:54:22-07:00",
+          "tree_id": "29ad48dffd9d11615f87549860c2ae5de307fe73",
+          "url": "https://github.com/jenniferlianne/cacti/commit/891a70567243eb96879680e3deea1136d9ad654c"
+        },
+        "date": 1729781986322,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 581,
+            "range": "±1.75%",
+            "unit": "ops/sec",
+            "extra": "177 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 695,
+            "range": "±3.28%",
+            "unit": "ops/sec",
+            "extra": "181 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
