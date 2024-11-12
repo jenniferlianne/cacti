edsonCVN/cacti-edson-fork

feat(copm): add Corda COPM implementation
Primary change:

Implements COPM primitives for Corda as a cacti
plugin.  The implementation follows the model of
the Corda ledger connector plugin, where a
typescript pass-through implementation is
registered on the plugin server, and commands are
implemented on a separate grpc server in the
Kotlin Spring framework.

Secondary changes:

- The lock claim protobuf structure was updated
to reduce the number of parameters.
- A no-op endpoint was added, per project
specification
- The format of the inter-network command for
requesting the status of a pledge varies by
remote network type and asset.  Adds a function to
the typescript interop configuration  interface to
supply the appropriate command given
the remote network and asset.
- Adds test package to test combinations of
network types
- Updates CI script to use new testing
framework
- Updates linter ignore to include weaver build
directories, allowing yarn lint to pass on
a system where weaver libraries have been built.
Also excludes docs directory to avoid linting
minified js from documentation packages.

Signed-off-by: Jennifer Bell <jenniferlianne@gmail.com>

packages/cacti-plugin-copm-corda/src/main/kotlin/org/hyperledger/cacti/plugin/copm/interop/corda/ViewAddressCorda.kt
@@ -1,0 +1,18 @@
+package org.hyperledger.cacti.plugin.copm.interop.corda
+
+import org.hyperledger.cacti.plugin.copm.types.DLTransactionParams
+import org.hyperledger.cacti.plugin.copm.interop.RemoteOrgConfig
+import org.hyperledger.cacti.plugin.copm.interop.ViewAddressFormat
+import org.hyperledger.cacti.weaver.sdk.corda.InteroperableHelper
+
+class ViewAddressCorda(private val remoteOrgConfig: RemoteOrgConfig): ViewAddressFormat {
+    override fun address(cmd: DLTransactionParams): String {
+        return InteroperableHelper.createCordaViewAddress(
+            this.remoteOrgConfig.networkId,
+            this.remoteOrgConfig.relayEndpoint,
+            this.remoteOrgConfig.partyEndpoints,
+            cmd.contract + "." + cmd.method,
+            cmd.args.joinToString(separator=":") { it.toString() })
+    }
+
+}

packages/cacti-plugin-copm-corda/src/main/kotlin/org/hyperledger/cacti/plugin/copm/interop/fabric/ViewAddressFabric.kt
@@ -1,0 +1,18 @@
+package org.hyperledger.cacti.plugin.copm.interop.fabric
+
+import org.hyperledger.cacti.plugin.copm.types.DLTransactionParams
+import org.hyperledger.cacti.plugin.copm.interop.RemoteOrgConfig
+import org.hyperledger.cacti.plugin.copm.interop.ViewAddressFormat
+import org.hyperledger.cacti.weaver.sdk.corda.InteroperableHelper
+
+class ViewAddressFabric(private val remoteOrgConfig: RemoteOrgConfig): ViewAddressFormat {
+    override fun address(cmd: DLTransactionParams): String {
+        return InteroperableHelper.createFabricViewAddress(
+            this.remoteOrgConfig.networkId,
+            this.remoteOrgConfig.relayEndpoint,
+            this.remoteOrgConfig.channelName,
+            cmd.contract,
+            cmd.method,
+            cmd.args.joinToString(separator=":") { it.toString() })
+    }
+}

jenniferlianne/cacti

packages/cacti-plugin-copm-corda/src/main/kotlin/org/hyperledger/cacti/plugin/copm/interop/corda/ViewAddressCorda.kt
@@ -1,0 +1,18 @@
+package org.hyperledger.cacti.plugin.copm.interop.corda
+
+import org.hyperledger.cacti.plugin.copm.types.DLTransactionParams
+import org.hyperledger.cacti.plugin.copm.interop.RemoteNetworkConfig
+import org.hyperledger.cacti.plugin.copm.interop.ViewAddressFormat
+import org.hyperledger.cacti.weaver.sdk.corda.InteroperableHelper
+
+class ViewAddressCorda(private val remoteNetworkConfig: RemoteNetworkConfig): ViewAddressFormat {
+    override fun address(cmd: DLTransactionParams): String {
+        return InteroperableHelper.createCordaViewAddress(
+            this.remoteNetworkConfig.networkId,
+            this.remoteNetworkConfig.relayEndpoint,
+            this.remoteNetworkConfig.partyEndpoints,
+            cmd.contract + "." + cmd.method,
+            cmd.args.joinToString(separator=":") { it.toString() })
+    }
+
+}

packages/cacti-plugin-copm-corda/src/main/kotlin/org/hyperledger/cacti/plugin/copm/interop/fabric/ViewAddressFabric.kt
@@ -1,0 +1,18 @@
+package org.hyperledger.cacti.plugin.copm.interop.fabric
+
+import org.hyperledger.cacti.plugin.copm.types.DLTransactionParams
+import org.hyperledger.cacti.plugin.copm.interop.RemoteNetworkConfig
+import org.hyperledger.cacti.plugin.copm.interop.ViewAddressFormat
+import org.hyperledger.cacti.weaver.sdk.corda.InteroperableHelper
+
+class ViewAddressFabric(private val remoteNetworkConfig: RemoteNetworkConfig): ViewAddressFormat {
+    override fun address(cmd: DLTransactionParams): String {
+        return InteroperableHelper.createFabricViewAddress(
+            this.remoteNetworkConfig.networkId,
+            this.remoteNetworkConfig.relayEndpoint,
+            this.remoteNetworkConfig.channelName,
+            cmd.contract,
+            cmd.method,
+            cmd.args.joinToString(separator=":") { it.toString() })
+    }
+}